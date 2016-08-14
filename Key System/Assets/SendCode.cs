28c1ab4389304df771d55fc4544053592839cf32
Mechanolith <j.kuskoff@hotmail.com>
2016-08-14 19:59:51 +1000

    Created Code Sending Framework

---
+++ Key System/Assets/SendCode.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendCode : MonoBehaviour {
+    public string testCode;
+    public string url = "http://www.handsomedragongames.com/KeyTest.php"; //Need to update this to the correct address. Also make it changable.
+	
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.P))
+        {
+            SendToServer(testCode, url);
+        }
+	}
+
+    public void SendToServer(string code, string targetURL) 
+    {
+        WWWForm outForm = new WWWForm();
+        string steamID = "";
+        //Get steamID.
+
+        //Send both to server.
+        outForm.AddField("Key", code);
+        outForm.AddField("ID", steamID);
+        WWW www = new WWW(targetURL, outForm);
+
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www) 
+    {
+        yield return www;
+
+        //Error Check
+        if (www.error == null)
+        {
+            Debug.Log("Post Request SUCCESS!");
+            ParseResults(www);
+        }
+        else 
+        {
+            Debug.LogWarning("Post Request FAILED!");
+        }
+    }
+
+    //See if the code was found successfully.
+    //If it was, distribute the correct reward.
+    void ParseResults(WWW www) 
+    {
+ 
+    }
+}
